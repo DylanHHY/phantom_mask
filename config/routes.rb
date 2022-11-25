12e32d78409c3b6f93cf24f2bcbeed32c9973ca0
dylan <dyan2009200@gmail.com>
2022-11-25 15:17:46 +0800

    完成前兩項routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,21 @@ Rails.application.routes.draw do
     namespace :v1 do 
       resources :pharmacies do 
         collection do 
+          # 星期及特定時間搜尋有營業的藥局
           get :open_at_day
           get :day_of_week
-
         end
+
+        member do
+          # 搜尋特定藥局所販售的口罩
+          get :get_masks
+        end
+      end
+
+      resources :masks do 
+      end
+
+      resources :transaction do
       end
     end
   end
